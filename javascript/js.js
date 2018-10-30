d93a56b84c87342421f985db40f2a6c3f33f41c9
sumina <suminaxresta@gmail.com>
2018-10-30 09:35:11 +0545

    capitalize

--- javascript/js.js
+++ javascript/js.js
@@ -28,6 +28,39 @@ const palindrome = (str) => {
     // }
 }
 
+// const reverseInt = (num) => {
+// let reverse = 0, divnum = 0;
+// while(num > 0 )
+// {
+//   divnum = num % 10;
+//   reverse = (reverse * 10) + divnum;
+//   num = num/10;
+// }
+// return reverse;
+
+//another method:
+
+// }
+let word = "";
+function capitalLetters(sen)
+{
+    word = sen.toLowerCase();
+    console.log("1 ",word);
+    let splitString = word.split(" ");
+    console.log("2 ", splitString);
+    for(let i=0; i<splitString.length; i++)
+    {
+        splitString[i]=splitString[i].charAt(0).toUpperCase()+ splitString[i].slice(1);
+
+        console.log("3 ", splitString[i]);
+    }
+
+    return splitString.join(" ");
+}
+
 console.log(stringReversal('hello'));
 console.log(palindrome('racecar'));
+// console.log(reverseInt(513));
+// console.log(word);
+console.log(capitalLetters("donot RepeaT yourself"));
 
